febd1a44e061a970200971ae9576799083688cc9
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2016-10-14 13:29:35 +0700

    Added implementation of ModelSignalProcessorMixin.teardown() method which disonnects own signal handlers.

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -4,7 +4,18 @@ from __future__ import absolute_import
 
 from django.db.models import signals
 
+import haystack
+
 
 def test_signal_handlers_registered():
     assert len(signals.post_save.receivers) > 0
     assert len(signals.post_delete.receivers) > 0
+
+def test_signal_handlers_cleaned_on_teardown():
+    original_class_prepared_handlers_length = len(signals.class_prepared.receivers)
+    haystack.signal_processor.teardown()
+    assert len(signals.post_save.receivers) == 0
+    assert len(signals.post_delete.receivers) == 0
+    expected = original_class_prepared_handlers_length - 1
+    assert len(signals.class_prepared.receivers) == expected
+    haystack.signal_processor.setup()
